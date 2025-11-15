d933587ba7e5feeddc6c034663ff4bf8638889e3
karlariquer <kriquer@gmail.com>
2025-11-15 18:44:43 +0100

    Working..

--- Project.py
+++ Project.py
@@ -1,3 +1,13 @@
 #The start to my project
+
+import deck_of_cards
+import random
+
+
 #hello
 print("Hello World")
+user_name = input("What do you want to be called? ")
+
+print(f"Nice. Now let's play some Pedrito {user_name}! Don't forget to call Pedrito "
+      f"when you think you have the least amount of points.")
+
---
+++ deck_of_cards.py
@@ -0,0 +1,53 @@
+import random
+
+class Card:
+    #makes a single card
+    def __init__(self, suit, number):
+        self.suit = suit
+        self.number= number
+
+    def __str__(self):
+        return f"{self.number} of {self.suit}"
+
+    def __repr__(self):
+        return str(self)
+
+# #test for Card Class
+# print(Card("Cups", 2))
+
+class Deck:
+    # makes the deck of cards, 4 suits "Cups, Clubs, Coins and Swords" with numbers 1-12
+    def __init__(self):
+        suits = ["Cups", "Clubs", "Coins", "Swords"]
+        numbers = []
+        for n in range(1, 13):
+            numbers.append(n)
+        #Create list of cards in deck
+        self.cards = [Card(suit, number) for suit in suits for number in numbers]
+
+        #Test to show the deck
+        # print(self.cards)
+
+    def __str__(self):
+        return f"A Spanish Deck with {len(self.cards)} cards remaining."
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def deal(self, num_cards):
+        if num_cards > len(self.cards):
+            print("There aren't sufficient cards")
+            return None
+        dealt_cards = self.cards[:num_cards]
+        self.cards = self.cards[num_cards:]
+        return dealt_cards
+
+# #test for Deck Class
+# spanish_deck= Deck()
+# print(spanish_deck)
+
+# #Test for deal method
+# spanish_deck.deal(4)
+# print (spanish_deck.deal(4))
+
+
